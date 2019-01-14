9082de657d063dcd16a44bccc22b7199671fdb53
Olav Schettler <olav@schettler.net>
2019-01-14 15:33:28 +0100

    Navbar as partial, converted to BS4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=">
     <meta name="google-site-verification" content="{{env('GOOGLE_SITE')}}" />
     <meta name="google-site-verification" content="{{env('GOOGLE_SITE2')}}" />
 
@@ -33,98 +33,7 @@
 </head>
 <body class="{{ str_replace('.', '-', Route::currentRouteName()) }}{{ Route::currentRouteName() != 'home' ? ' not-home' : '' }}">
     @section('navbar')
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                    @if (Auth::check())
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Concepts <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{route('concept.index')}}">All</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="{{route('concept.toplevel')}}">Toplevel</a></li>
-                                <li><a href="{{route('concept.popular')}}">Popular</a></li>
-                                <li><a href="{{route('concept.flagged')}}">Flagged</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="{{route('tags.cloud')}}">Tags</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="{{route('concept.shares')}}">Shared by me</a></li>
-                                <li><a href="{{route('concept.shared')}}">Shared with me</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="{{route('concept.create')}}"><i class="glyphicon glyphicon-plus-sign"></i> New concept</a></li>
-                            </ul>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Tasks <span class="caret"></span></a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{route('item.todo')}}">Open</a></li>
-                                <li><a href="{{route('item.done')}}">Completed</a></li>
-                            </ul>
-                        </li>
-
-                        <li><a href="{{ route('journal') }}"><i class="glyphicon glyphicon-grain"></i> {{ strftime('%Y-%m-%d') }}</a></li>
-                    @endif
-                    </ul>
-
-                    @if (Auth::check() && Route::currentRouteName() != 'home')
-                        @include('core::partials.search-form', ['class' => 'desktop-only navbar-form navbar-left'])
-                    @endif
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a id="generate-token" href="#">API-Token</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('user.passport') }}">Passport</a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        @include('core::partials.navbar')
     @show
 
     @yield('content')
