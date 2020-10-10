37402799c48d90adb882043065a9cd3d38fdc7e5
Olav Schettler <olav.schettler@dw.com>
2020-10-10 11:35:23 +0200

    Status 2020-10-10

---
+++ migrations/2017_02_18_145707_create_concepts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Kalnoy\Nestedset\NestedSet;
+
+class CreateConceptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('concepts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('summary')->nullable();
+
+            $table->longText('body')->nullable();
+            $table->string('body_format')->default('markdown');
+
+            $table->string('language')->default('de');
+            $table->integer('translation_id')->unsigned()->nullable();
+            $table->foreign('translation_id')->references('id')->on('concepts');
+
+            NestedSet::columns($table);
+
+            $table->string('source_url')->nullable();
+
+            $table->bigInteger('owner_id')->unsigned();
+            $table->foreign('owner_id')->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('concepts');
+    }
+}
